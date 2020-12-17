66e9819f795cd6ea130276a2a26e969bbaa70024
Charles Fourneau <charles.fourneau@gmail.com>
2020-12-17 10:25:36 +0100

    2020.17 - n-dimensions generalization

--- 2020/17/day_17.py
+++ 2020/17/day_17.py
@@ -1,110 +1,90 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import re
-from collections import defaultdict
 from itertools import product
 from copy import deepcopy
+
+class Space:
+    def __init__(self, n=3, default=0):
+        self.n = n
+        self.space = {}
+        self.default = default
+        self.extent = [(0,0)] * self.n
+    
+    def set(self, coords, value, allow_extend=False):
+        l = len(coords)
+        if l == self.n:
+            c = coords
+        elif l < self.n:
+            if allow_extend:
+                c = tuple(list(coords) + [0]*(self.n - l))
+            else:
+                raise ValueError('Not enough dimensions: {}, expected {}'.format(l, self.n))
+        else:
+            raise ValueError('Too many dimensions: {}, expected {}'.format(l, self.n))
+        self.space[c] = value
+        for i in range(self.n):
+            self.extent[i] = (min(c[i], self.extent[i][0]),
+                              max(c[i], self.extent[i][1]))
+    
+    def get(self, coords):
+        # TODO: check coords
+        return self.space.get(coords, self.default)
+    
+    def is_set(self, coords):
+        return coords in self.space
+    
+    def is_in(self, coords):
+        return all(coords[i] in range(self.extent[i][0],self.extent[i][1]+1) for i in range(self.n))
+    
+    def count(self, value):
+        return list(self.space.values()).count(value)
+    
+    def add_dimension(self, n):
+        space_old = self.space.copy()
+        self.space = {}
+        self.n += n
+        self.extent = [(0,0)] * self.n
+        for x in space_old.items():
+            self.set(x[0], x[1], True)
+
+def conway_cubes(space, dimensions=3, loop=6):
+    space = deepcopy(space)
+    if space.n < dimensions:
+        space.add_dimension(dimensions - space.n)
+    directions = [d for d in product([-1,0,1], repeat=dimensions) if d != (0,)*dimensions]
+    for i in range(loop):
+        space_new = deepcopy(space)
+        for c in product(*(range(e[0]-1,e[1]+2) for e in space.extent)):
+            n_active = 0
+            for d in directions:
+                nc = tuple(c[i]+d[i] for i in range(dimensions))
+                if space.is_set(nc) and space.get(nc) == '#':
+                    n_active += 1
+            if space.is_set(c) and space.get(c) == '#':
+                if n_active not in (2,3):
+                    space_new.set(c,'.')
+            else:
+                if n_active == 3:
+                    space_new.set(c, '#')
+        space = space_new
+    return space.count('#')
+
 
 input_file = "input"
 #input_file = "test1.txt"
 
 if __name__ == '__main__':
-    count1, count2 = 0, 0
-
-    space = {}
-    x,y,z,w = 0,0,0,0
-    xmin, xmax = 0,0
-    ymin, ymax = 0,0
-    zmin, zmax = 0,0
-    wmin, wmax = 0,0
+    space = Space(2)
+    x,y = 0,0
 
     with open(input_file,'r') as f:
         for r in [l.strip() for l in f.readlines() if l.strip()]:
-            ymax = y
             for c in r:
-                xmax = x
-                space[(x,y,z)] = c
+                space.set((x,y), c)
                 x += 1
             y+= 1
             x = 0
-    
-    initial_space = deepcopy(space)
-    directions = [d for d in product([-1,0,1], repeat=3) if d != (0,0,0)]
 
-    for i in range(6):
-        space_new = deepcopy(space)
-        for x in range(xmin-1, xmax+2):
-            for y in range(ymin-1, ymax+2):
-                for z in range(zmin-1, zmax+2):
-                    n_active = 0
-                    for d in directions:
-                        nx = x + d[0]
-                        ny = y + d[1]
-                        nz = z + d[2]
-                        if (nx,ny,nz) in space and space[(nx,ny,nz)] == '#':
-                            n_active += 1
-                    # print(x,y,z," - ",n_active)
-                    if (x,y,z) in space and space[(x,y,z)] == '#':
-                        if n_active in (2,3):
-                            pass
-                        else:
-                            space_new[(x,y,z)] = '.'
-                    else:
-                        if n_active == 3:
-                            space_new[(x,y,z)] = '#'
-                            xmin = min(x,xmin)
-                            xmax = max(x,xmax)
-                            ymin = min(y,ymin)
-                            ymax = max(y,ymax)
-                            zmin = min(z,zmin)
-                            zmax = max(z,zmax)
-        space = space_new
-
-    count1 = list(space.values()).count('#')
-
-    # part 2
-    directions = [d for d in product([-1,0,1], repeat=4) if d != (0,0,0,0)]
-    
-    space = {}
-    for c in initial_space.items():
-        coords = tuple(list(c[0])+[0])
-        space[coords] = c[1]
-
-    for i in range(6):
-        space_new = deepcopy(space)
-        for x in range(xmin-1, xmax+2):
-            for y in range(ymin-1, ymax+2):
-                for z in range(zmin-1, zmax+2):
-                    for w in range(wmin-1,wmax+2):
-                        n_active = 0
-                        for d in directions:
-                            nx = x + d[0]
-                            ny = y + d[1]
-                            nz = z + d[2]
-                            nw = w + d[3]
-                            if (nx,ny,nz,nw) in space and space[(nx,ny,nz,nw)] == '#':
-                                n_active += 1
-                        # print(x,y,z," - ",n_active)
-                        if (x,y,z,w) in space and space[(x,y,z,w)] == '#':
-                            if n_active in (2,3):
-                                pass
-                            else:
-                                space_new[(x,y,z,w)] = '.'
-                        else:
-                            if n_active == 3:
-                                space_new[(x,y,z,w)] = '#'
-                                xmin = min(x,xmin)
-                                xmax = max(x,xmax)
-                                ymin = min(y,ymin)
-                                ymax = max(y,ymax)
-                                zmin = min(z,zmin)
-                                zmax = max(z,zmax)
-                                wmin = min(w,wmin)
-                                wmax = max(w,wmax)
-        space = space_new
-
-    count2 = list(space.values()).count('#')
-
-    print("Step 1:", count1)
-    print("Step 2:", count2)
+    print("Step 1:", conway_cubes(space, 3))
+    print("Step 2:", conway_cubes(space, 4))
