a73c177f9c99fab987da4457481333f96459ede8
Charles Fourneau <charles.fourneau@gmail.com>
2020-12-17 13:45:38 +0100

    2020.17 - first optimization attempt

--- 2020/17/day_17.py
+++ 2020/17/day_17.py
@@ -4,6 +4,7 @@
 from itertools import product
 from copy import deepcopy
 from datetime import datetime
+from operator import add
 
 class Space:
     def __init__(self, n=3, default=0):
@@ -22,6 +23,11 @@ class Space:
     def get(self, coords):
         coords = self._check_coords(coords)
         return self.space.get(coords, self.default)
+    
+    def remove(self, coords):
+        coords = self._check_coords(coords)
+        if self.is_set(coords):
+            del self.space[coords]
     
     def is_set(self, coords):
         coords = self._check_coords(coords)
@@ -42,13 +48,16 @@ class Space:
         for x in space_old.items():
             self.set(x[0], x[1], True)
     
+    def get_active_coords(self):
+        return list(self.space.keys())
+    
     def _check_coords(self, coords, allow_extend=False):
         l = len(coords)
         if l == self.n:
             pass
         elif l < self.n:
             if allow_extend:
-                coords = tuple(list(coords) + [0]*(self.n - l))
+                coords = (coords) + (0,)*(self.n - l)
             else:
                 raise ValueError('Not enough dimensions: {}, expected {}'.format(l, self.n))
         else:
@@ -61,16 +70,28 @@ def conway_cubes(space, dimensions=3, loop=6):
         space.add_dimension(dimensions - space.n)
     directions = [d for d in product([-1,0,1], repeat=dimensions) if d != (0,)*dimensions]
     for i in range(loop):
-        space_new = deepcopy(space)
-        for c in product(*(range(e[0]-1,e[1]+2) for e in space.extent)):
+        space_new = Space(dimensions)
+        # space_new = deepcopy(space)
+        # for c in product(*(range(e[0]-1,e[1]+2) for e in space.extent)):
+        to_check = set()
+        for x in space.get_active_coords():
+            to_check.add(x)
+            for d in directions:
+                # to_check.add(tuple(x[i]+d[i] for i in range(dimensions)))
+                to_check.add(tuple(map(add, x, d)))
+        for c in to_check:
             n_active = 0
             for d in directions:
-                nc = tuple(c[i]+d[i] for i in range(dimensions))
+                # nc = tuple(c[i]+d[i] for i in range(dimensions))
+                nc = tuple(map(add, c, d))
                 if space.is_set(nc) and space.get(nc) == '#':
                     n_active += 1
             if space.is_set(c) and space.get(c) == '#':
-                if n_active not in (2,3):
-                    space_new.set(c,'.')
+                # if n_active not in (2,3):
+                #     # space_new.set(c,'.')
+                #     space_new.remove(c)
+                if n_active in (2,3): # if new_space is empty (not a copy)
+                    space_new.set(c, '#')
             else:
                 if n_active == 3:
                     space_new.set(c, '#')
@@ -88,7 +109,8 @@ if __name__ == '__main__':
     with open(input_file,'r') as f:
         for r in [l.strip() for l in f.readlines() if l.strip()]:
             for c in r:
-                space.set((x,y), c)
+                if c == '#':
+                    space.set((x,y), c)
                 x += 1
             y+= 1
             x = 0
@@ -97,6 +119,6 @@ if __name__ == '__main__':
     # print("Step 2:", conway_cubes(space, 4))
 
     # let's do some measurements...
-    for n in range(2,5):
+    for n in range(2,6):
         start_time = datetime.now()
         print("n = {}:".format(n), conway_cubes(space, n), " -> ", datetime.now() - start_time)
