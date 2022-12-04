dbb84918d0816077119cd0adfadb3eb0eaf2566f
Charles Fourneau <charles.fourneau@gmail.com>
2022-12-04 07:10:55 +0100

    2022.04

---
+++ 2022/04/day_04.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+input_file = "input"
+#input_file = "test01.txt"
+
+if __name__ == '__main__':
+    overlap = 0
+    included = 0
+    for l in open(input_file).readlines():
+        p = l.strip().split(',')
+        c1,c2 = [[int(x) for x in p[i].split('-')] for i in range(2)] 
+        if ((c1[0] <= c2[0]) and (c1[1] >= c2[1])) \
+            or ((c2[0] <= c1[0]) and (c2[1] >= c1[1])):
+                included += 1
+        if max(c1[0], c2[0]) <= min(c1[1], c2[1]):
+            overlap += 1
+    print("Part #1 :", included)
+    print("Part #2 :", overlap)
