664a4aaec8f7a8ea3b81ce4984b2ca87f79ef550
Charles Fourneau <charles.fourneau@gmail.com>
2022-12-05 20:27:43 +0100

    2022.05 - Cleanup

--- 2022/05/day_05.py
+++ 2022/05/day_05.py
@@ -8,7 +8,7 @@ input_file = "input"
 #input_file = "test01.txt"
 
 pattern_move = re.compile(r"move (\d+) from (\d+) to (\d+)")
-pattern_crate = re.compile(r"\[([A-Z])\]|[ ]{3,4}")
+pattern_crate = re.compile(r"\[([A-Z])\] ?|[ ]{3,4}")
 
 if __name__ == '__main__':
     with open(input_file) as f:
@@ -16,21 +16,23 @@ if __name__ == '__main__':
     c = a.split("\n")
     n = len(c.pop().strip().split())
     stacks = [[] for _ in range(n)]
-    for d in reversed(c):
+    for d in reversed(c): # crates stacks
         y = pattern_crate.findall(d)
         for i in range(len(y)):
             if len(y[i]):
                 stacks[i].append(y[i])
     stacks2 = deepcopy(stacks)
-    for l in b.split("\n"):
-        z = pattern_move.match(l)
-        m = tuple(map(int, z.groups()))
+    for m in b.split("\n"): # moves
+        z = pattern_move.match(m)
+        n = int(z.group(1))     # number of crates
+        f = int(z.group(2)) - 1 # from
+        t = int(z.group(3)) - 1 # to
         # part 1
-        for i in range(m[0]):
-            stacks[m[2]-1].append(stacks[m[1]-1].pop())
+        for i in range(n):
+            stacks[t].append(stacks[f].pop())
         # part 2
-        stacks2[m[2]-1] += stacks2[m[1]-1][-m[0]:]
-        stacks2[m[1]-1] = stacks2[m[1]-1][:-m[0]]
+        stacks2[t] += stacks2[f][-n:]
+        stacks2[f] = stacks2[f][:-n]
 
     print("Part #1 :", "".join((s[-1] for s in stacks)))
     print("Part #2 :", "".join((s[-1] for s in stacks2)))
