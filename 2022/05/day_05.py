ea559d6b0fe567d78d553ee4a56694bedacaf6e0
Charles Fourneau <charles.fourneau@gmail.com>
2022-12-05 17:00:02 +0100

    2022.05

---
+++ 2022/05/day_05.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+from copy import deepcopy
+
+input_file = "input"
+#input_file = "test01.txt"
+
+pattern_move = re.compile(r"move (\d+) from (\d+) to (\d+)")
+pattern_crate = re.compile(r"\[([A-Z])\]|[ ]{3,4}")
+
+if __name__ == '__main__':
+    with open(input_file) as f:
+        a, b = f.read().rstrip().split("\n\n")
+    c = a.split("\n")
+    n = len(c.pop().strip().split())
+    stacks = [[] for _ in range(n)]
+    for d in reversed(c):
+        y = pattern_crate.findall(d)
+        for i in range(len(y)):
+            if len(y[i]):
+                stacks[i].append(y[i])
+    stacks2 = deepcopy(stacks)
+    for l in b.split("\n"):
+        z = pattern_move.match(l)
+        m = tuple(map(int, z.groups()))
+        # part 1
+        for i in range(m[0]):
+            stacks[m[2]-1].append(stacks[m[1]-1].pop())
+        # part 2
+        stacks2[m[2]-1] += stacks2[m[1]-1][-m[0]:]
+        stacks2[m[1]-1] = stacks2[m[1]-1][:len(stacks2[m[1]-1])-m[0]]
+
+    print("Part #1 :", "".join((s[-1] for s in stacks)))
+    print("Part #2 :", "".join((s[-1] for s in stacks2)))
