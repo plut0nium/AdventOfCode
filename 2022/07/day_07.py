292e29fc37eabd73084af7aa95bec9305c750426
Charles Fourneau <charles.fourneau@gmail.com>
2022-12-07 09:26:38 +0100

    2022.07

--- 2022/07/day_07.py
+++ 2022/07/day_07.py
@@ -2,40 +2,56 @@
 # -*- coding: utf-8 -*-
 
 input_file = "input"
-input_file = "test01.txt"
+#input_file = "test01.txt"
+
+FS_SIZE = 70000000
+SPACE_REQUIRED = 30000000
 
 def dir_size(d, fs):
     s = 0
     for f in fs[d]:
         if isinstance(f, str):
-            s += dir_size(f, fs)
+            # next dir
+            nd = d + "/" + f if len(d) > 1 else d + f
+            s += dir_size(nd, fs)
         else:
             s += f
     return s
 
 if __name__ == '__main__':
     fs = dict()
-    current_dir = None
+    current_path = []
     for c in open(input_file).readlines():
         c = c.strip()
         if c[:4] == '$ cd':
-            current_dir = c[5:]
+            if c[5:] == '..':
+                current_path.pop()
+            elif c[5:] == '/':
+                current_path = ['/']
+            else:
+                current_path.append(c[5:])
         elif c[:4] == '$ ls':
             pass
         else:
-            if current_dir not in fs:
-                fs[current_dir] = []
+            p = current_path[0] + "/".join(current_path[1:])
+            if p not in fs:
+                fs[p] = []
             t, n = c.split()
             if t == 'dir':
-                fs[current_dir].append(n)
+                fs[p].append(n)
             else:
-                fs[current_dir].append(int(t))
+                fs[p].append(int(t))
+            pass
 
     t = 0
+    unused_space = FS_SIZE - dir_size("/", fs)
+    candidate = FS_SIZE
     for k in fs.keys():
         s = dir_size(k, fs)
         if s <= 100000:
             t += s
+        if s >= (SPACE_REQUIRED - unused_space) and s < candidate:
+            candidate = s
 
     print("Part #1 :", t)
-    # print("Part #2 :", None)
+    print("Part #2 :", candidate)
