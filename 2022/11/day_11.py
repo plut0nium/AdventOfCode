06369313d3de421b7d2914c3daac1588acba6f18
Charles Fourneau <charles.fourneau@gmail.com>
2022-12-11 12:11:58 +0100

    2022.11 - Cleanup

--- 2022/11/day_11.py
+++ 2022/11/day_11.py
@@ -28,15 +28,35 @@ def parse_monkey(m):
         o1 = math.prod
     else:
         print('Error - Unknown operation')
-    if m[2][25:] == 'old':
-        o2 = 'old'
-    else:
+    try:
         o2 = int(m[2][25:])
+    except ValueError:
+        # we assume only alternative is 'old'
+        o2 = 'old'
     op = (o1, o2)
     test = int(m[3][21:])
     dest_true = int(m[4][29:])
     dest_false = int(m[5][30:])
     return Monkey(items, op, test, dest_true, dest_false)
+
+def monkey_in_the_middle(monkeys, rounds, part=1):
+    if part != 1:
+        d = math.prod((m.test for m in monkeys)) # m.test are all primes
+    for r in range(rounds):
+        for m in monkeys:
+            for i in m.items:
+                i = m.op[0]((i, m.op[1] if isinstance(m.op[1],int) else i))
+                if part == 1:
+                    i = i//3
+                else:
+                    i = i % d # keep worry levels manageable
+                if i % m.test == 0:
+                    monkeys[m.dest_true].items.append(i)
+                else:
+                    monkeys[m.dest_false].items.append(i)
+                m.count += 1
+            m.items.clear()
+    return sorted([m.count for m in monkeys])
 
 if __name__ == '__main__':
     monkeys = []
@@ -44,40 +64,11 @@ if __name__ == '__main__':
         m = f.read().strip().split('\n\n')
     for m_ in m:
         monkeys.append(parse_monkey(m_))
-        
+
     monkeys2 = deepcopy(monkeys)
 
-    # part 1
-    for r in range(ROUNDS):
-        for m in monkeys:
-            for i in m.items:
-                i = m.op[0]((i, i if m.op[1]=='old' else m.op[1]))
-                i = i//3
-                if i % m.test == 0:
-                    monkeys[m.dest_true].items.append(i)
-                else:
-                    monkeys[m.dest_false].items.append(i)
-                m.count += 1
-            m.items = []
-
-    c = sorted([m.count for m in monkeys])
-
-    # part 2
-    d = math.prod((m.test for m in monkeys2))
-    for r in range(ROUNDS2):
-        for m in monkeys2:
-            for i in m.items:
-                i = m.op[0]((i, i if m.op[1]=='old' else m.op[1]))
-                #i = i//3
-                i = i % d
-                if i % m.test == 0:
-                    monkeys2[m.dest_true].items.append(i)
-                else:
-                    monkeys2[m.dest_false].items.append(i)
-                m.count += 1
-            m.items = []
-
-    c2 = sorted([m.count for m in monkeys2])
+    c = monkey_in_the_middle(monkeys, ROUNDS)
+    c2 = monkey_in_the_middle(monkeys2, ROUNDS2, part=2)
     
     print("Part #1 :", math.prod(c[-2:]))
     print("Part #2 :", math.prod(c2[-2:]))
