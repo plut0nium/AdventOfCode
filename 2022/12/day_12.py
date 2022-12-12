15922b98e73ece700aff78c41e3e4cbcb31e23c8
Charles Fourneau <charles.fourneau@gmail.com>
2022-12-12 09:44:54 +0100

    2022.12 - Allow solving parts 1 & 2 in a single pass, by starting from the top

--- 2022/12/day_12.py
+++ 2022/12/day_12.py
@@ -9,7 +9,28 @@ input_file = "input"
 START = ord('S')
 END = ord('E')
 
-move = ((1, 0), (-1, 0), (0, -1), (0, 1))
+moves = ((1, 0), (-1, 0), (0, -1), (0, 1))
+
+def hill_climbing_algorithm(start_coord, height_map):
+    """ More a Hill Descending Algorithm """
+    visited = {}
+    candidate = [(start_coord, 0)]
+    while len(candidate):
+        current, steps = candidate.pop()
+        for m in moves:
+            dest = (current[0]+m[0], current[1]+m[1])
+            if -1 in dest \
+                or dest[0] >= height_map.shape[0] \
+                or dest[1] >= height_map.shape[1]:
+                # out of map
+                continue
+            if height_map[dest] < height_map[current]-1:
+                # cannot climb if step > 1 in the ascending direction
+                continue
+            if dest not in visited or visited[dest] > steps+1:
+                candidate.append((dest, steps+1))
+        visited[current] = steps
+    return visited
 
 if __name__ == '__main__':
     with open(input_file, 'r') as f:
@@ -24,47 +45,14 @@ if __name__ == '__main__':
     height_map = np.array(m)
     height_map[start_coord] = ord('a')
     height_map[end_coord] = ord('z')
-    visited = {}
-    candidate = [(start_coord, 0)]
 
-    while len(candidate):
-        current, steps = candidate.pop()
-        for n in move:
-            dest = (current[0]+n[0], current[1]+n[1])
-            if -1 in dest \
-                or dest[0] >= height_map.shape[0] \
-                or dest[1] >= height_map.shape[1]:
-                # out of map
-                continue
-            if height_map[dest] > height_map[current]+1:
-                # cannot climb
-                continue
-            if dest not in visited or visited[dest] > steps+1:
-                candidate.append((dest, steps+1))
-        visited[current] = steps
+    # start from the top
+    # allows for easily solving parts 1 & 2
+    visited = hill_climbing_algorithm(end_coord, height_map)
     
-    start_coord = end_coord
-    candidate = [(start_coord, 0)]
-    visited2 = {}
-
-    while len(candidate):
-        current, steps = candidate.pop()
-        for n in move:
-            dest = (current[0]+n[0], current[1]+n[1])
-            if -1 in dest \
-                or dest[0] >= height_map.shape[0] \
-                or dest[1] >= height_map.shape[1]:
-                # out of map
-                continue
-            if height_map[dest] < height_map[current]-1:
-                # cannot climb
-                continue
-            if dest not in visited2 or visited2[dest] > steps+1:
-                candidate.append((dest, steps+1))
-        visited2[current] = steps
-    
+    # potential starting points
     s = np.where(height_map == ord('a'))
-    ss = list(zip(s[0], s[1]))
+    starting_points = list(zip(s[0], s[1]))
 
-    print("Part #1 :", visited[end_coord])
-    print("Part #2 :", min((visited2[s] for s in ss if s in visited2)))
+    print("Part #1 :", visited[start_coord])
+    print("Part #2 :", min((visited[s] for s in starting_points if s in visited)))
