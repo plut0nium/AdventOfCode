56488ac5345964beddd540d31c2c4c11016c2e9c
Charles Fourneau <charles.fourneau@gmail.com>
2023-12-05 15:04:44 +0100

    2023.05 This was hard !

--- 2023/05/day_05.py
+++ 2023/05/day_05.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 input_file = "input"
-input_file = "test01.txt"
+# input_file = "test01.txt"
 # input_file = "test02.txt"
 
 def parse_almanac(almanac_list):
@@ -25,33 +25,79 @@ def parse_almanac(almanac_list):
         maps.append(current_map)
     return seeds, maps
 
-def get_position(s, almanac):
+def get_location(s, almanac):
     # print("Seed: ", s)
     new_s = s
     for a in almanac:
+        # loop through each map in the almanac
         for m in a:
+            # check for each mapped range
             source, dest, length = m
             if new_s >= source and new_s < source + length:
                 new_s = dest + (new_s - source)
                 # print("> ", new_s, end=" ")
                 break
-    # print("")
+            # if no range found, seed location is not updated
+    # print() # newline
     return new_s
-        
 
 def part1(seeds, almanac):
-    seed_position = []
+    seed_location = []
     for s in seeds:
-        seed_position.append(get_position(s, almanac))
-    return min(seed_position)
-    
-def part2(cards):
-    return None
+        seed_location.append(get_location(s, almanac))
+    return min(seed_location)
+
+def intersect(r1, r2):
+    return (max(r1[0],r2[0]), min(r1[1],r2[1])) \
+        if max(r1[0],r2[0]) <= min(r1[1],r2[1]) else None
+
+def difference(r1, r2):
+    # return r1 - r2
+    if intersect(r1, r2) is None:
+        return r1
+    diff = []
+    if r1[0] < r2[0]:
+        diff.append((r1[0], r2[0] - 1))
+    if r1[1] > r2[1]:
+        diff.append((r2[1] + 1, r1[1]))
+    return [d for d in diff]
+
+def part2(seeds, almanac):
+    # we have > 2E^10 seeds... let's avoid the brute force
+    # print(sum((seeds[i]) for i in range(1, len(seeds), 2)))
+    assert(len(seeds) % 2 == 0)
+    seed_map = [(seeds[i], seeds[i] + seeds[i+1] - 1)
+                    for i in range(0, len(seeds), 2)]
+
+    for j, a in enumerate(almanac):
+        # for each almanac "level"
+        mapped_seeds = [] # seeds mapped to a new location
+        for m in a:
+            # ...for each mapped range
+            source, dest, length = m
+            r_min, r_max = source, source + length - 1
+            r_move = dest - source
+            # seeds
+            not_mapped_seeds = [] # seeds that are not mapped (currently)
+            for s in seed_map:
+                s_min, s_max = s
+                i = intersect((s_min, s_max), (r_min, r_max))
+                if i is None:
+                    # no intersection -> seeds are not moved
+                    not_mapped_seeds.append((s_min, s_max))
+                    continue
+                # map seeds in range to new a location
+                mapped_seeds.append((i[0] + r_move, i[1] + r_move))
+                # keep remaining seeds (could be mapped by another range)
+                not_mapped_seeds.extend(difference((s_min, s_max), (r_min, r_max)))
+            # seeds can only be mapped by a single range at current almanac level
+            seed_map = not_mapped_seeds
+        seed_map = mapped_seeds + not_mapped_seeds
+    return min((s[0] for s in seed_map))
 
 
 if __name__ == '__main__':
-    almanac = None
     with open(input_file, 'r') as f:
         seeds, almanac = parse_almanac(f.readlines())
     print("Part #1 :", part1(seeds, almanac))
-    print("Part #2 :", part2(almanac))
+    print("Part #2 :", part2(seeds, almanac))
