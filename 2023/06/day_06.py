5581e796d136c4588d9a2ba76b6d2ce84a3dc461
Charles Fourneau <charles.fourneau@gmail.com>
2023-12-06 09:23:00 +0100

    2023.06 added an algebraic solution in part2

--- 2023/06/day_06.py
+++ 2023/06/day_06.py
@@ -6,6 +6,7 @@ input_file = "input"
 # input_file = "test02.txt"
 
 from functools import reduce
+import math
 
 def parse_races(races_str):
     races_str = races_str.split("\n")[:2]
@@ -31,14 +32,28 @@ def part1(races):
                 record_beaten[i] += 1
     return reduce(lambda x, y: x*y, record_beaten)
 
-def part2(race):
-    record_beaten = 0
-    for t in range(race[0]//2): # symmetrical -> can check for 1/2
-        d = t * (race[0] - t)
-        if d > race[1]:
-            # for all values > t, we will beat the record
-            record_beaten = (race[0] + 1) - t * 2
-            break
+def part2(race, brute=False):
+    if brute:
+        record_beaten = 0
+        for t in range(race[0]//2): # symmetrical -> can check for 1/2
+            d = t * (race[0] - t)
+            if d > race[1]:
+                # for all values > t, we will beat the record
+                record_beaten = (race[0] + 1) - t * 2
+                break
+    else:
+        # this equals to solve (find the roots of)
+        # the following quadratic equation
+        #   t ** 2 - t * race[0] + race[1] = 0
+        delta = race[0] ** 2 - 4 * race[1]
+        t1 = (race[0] - math.sqrt(delta)) / 2
+        t2 = (race[0] + math.sqrt(delta)) / 2
+        # assume t2 > t1
+        if t1.is_integer(): # this value will match the record but not beat it...
+            t1 += 1
+        if t2.is_integer():
+            t2 -= 1
+        record_beaten = math.floor(t2) - math.ceil(t1) + 1
     return record_beaten
 
 
