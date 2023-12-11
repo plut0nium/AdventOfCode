95643f97d2a916e0202166d1f9b96297a3f99e23
Charles Fourneau <charles.fourneau@gmail.com>
2023-12-11 12:44:45 +0100

    2023.11 simplify part 2 + use the same solution for part 1

--- 2023/11/day_11.py
+++ 2023/11/day_11.py
@@ -8,7 +8,6 @@ input_file = "input"
 GALAXY = "#"
 
 from itertools import combinations
-from copy import copy
 
 def manhattan(a, b):
     return abs(b[0] - a[0]) + abs(b[1] - a[1])
@@ -17,17 +16,11 @@ def manhattan_expand(a, b, galaxies, age=1):
     x_min, x_max = min(a[0], b[0]), max(a[0], b[0])
     y_min, y_max = min(a[1], b[1]), max(a[1], b[1])
     x_values, y_values = map(set, zip(*galaxies))
-    distance = 0
-    for x in range(x_min+1, x_max+1):
-        if x in x_values:
-            distance += 1
-        else:
-            distance += age
-    for y in range(y_min+1, y_max+1):
-        if y in y_values:
-            distance += 1
-        else:
-            distance += age
+    distance = manhattan(a, b)
+    for x in set(range(x_min, x_max+1)) - x_values:
+        distance += age - 1
+    for y in set(range(y_min, y_max+1)) - y_values:
+        distance += age - 1
     return distance
 
 def parse_galaxies(image_list):
@@ -38,40 +31,10 @@ def parse_galaxies(image_list):
                 galaxies.append((x,y))
     return galaxies
 
-def print_galaxies(galaxies):
-    x_values, y_values = map(set, zip(*galaxies))
-    x_min, x_max, y_min, y_max = min(x_values), max(x_values), \
-                                 min(y_values), max(y_values)
-    for y in range(y_min, y_max + 1):
-        for x in range(x_min, x_max + 1):
-            print("#" if (x,y) in galaxies else ".", end="")
-        print("")
-
-def expand(galaxies):
-    galaxies = copy(galaxies) # make a copy
-    x_values, y_values = map(set, zip(*galaxies))
-    x_min, x_max, y_min, y_max = min(x_values), max(x_values), \
-                                 min(y_values), max(y_values)
-    for y in range(y_max, y_min-1, -1): # expand backwards
-        if y in y_values:
-            # row not empty
-            continue
-        for i in range(len(galaxies)):
-            if galaxies[i][1] > y:
-                galaxies[i] = (galaxies[i][0], galaxies[i][1] + 1)
-    for x in range(x_max, x_min-1, -1):
-        if x in x_values:
-            # col not empty
-            continue
-        for i in range(len(galaxies)):
-            if galaxies[i][0] > x:
-                galaxies[i] = (galaxies[i][0] + 1, galaxies[i][1])
-    return galaxies
-
 def part1(galaxies):
     sum = 0
-    for c in combinations(expand(galaxies), 2):
-        sum += manhattan(*c)
+    for c in combinations(galaxies, 2):
+        sum += manhattan_expand(c[0], c[1], galaxies, 2)
     return sum
 
 def part2(galaxies):
@@ -79,7 +42,6 @@ def part2(galaxies):
     for c in combinations(galaxies, 2):
         sum += manhattan_expand(c[0], c[1], galaxies, 1_000_000)
     return sum
-
 
 
 if __name__ == '__main__':
