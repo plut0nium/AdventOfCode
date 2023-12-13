446b3e2e28289afc36e734f3317a9c0b60ec540d
Charles Fourneau <charles.fourneau@gmail.com>
2023-12-13 10:38:38 +0100

    2023.13 part 2

--- 2023/13/day_13.py
+++ 2023/13/day_13.py
@@ -8,7 +8,7 @@ input_file = "input"
 ASH = "."
 ROCK = "#"
 
-from collections import defaultdict
+from copy import copy
 
 def parse_patterns(input_lines):
     patterns = [{}]
@@ -43,38 +43,62 @@ def fold(p, v, y_axis=False):
     # folds pattern p along v|v+1 on x_axis, unless y_axis is True
     x_min, x_max, y_min, y_max = grid_size(p)
     if not y_axis:
-        if v not in range(x_min, x_max):
-            raise ValueError(f'Value {v} out of X-range: {x_min}-{x_max}')
+        # if v not in range(x_min, x_max):
+        #     raise ValueError(f'Value {v} out of X-range: {x_min}-{x_max}')
         extent = min((v+1)-x_min, x_max-v)
         return all(get_y_values(p, v-e) == get_y_values(p, (v+1)+e) for e in range(extent))
     else:
-        if v not in range(y_min, y_max):
-            raise ValueError(f'Value {v} out of Y-range: {y_min}-{y_max}')
+        # if v not in range(y_min, y_max):
+        #     raise ValueError(f'Value {v} out of Y-range: {y_min}-{y_max}')
         extent = min((v+1)-y_min, y_max-v)
         return all(get_x_values(p, v-e) == get_x_values(p, (v+1)+e) for e in range(extent))
     return False
 
-def part1(patterns):
+def find_reflection(p):
+    reflection_lines = []
+    x_min, x_max, y_min, y_max = grid_size(p)
+    for x in range(x_min, x_max):
+        if fold(p, x):
+            reflection_lines.append((x,None))
+    for y in range(y_min, y_max):
+        if fold(p, y, y_axis = True):
+            reflection_lines.append((None,y))
+    return reflection_lines
+
+def summarize(notes):
     total = 0
-    for i, p in enumerate(patterns):
-        is_folded = False
-        x_min, x_max, y_min, y_max = grid_size(p)
-        for x in range(x_min, x_max):
-            if fold(p, x):
-                total += x + 1
-                is_folded = True
-                break
-        if is_folded:
-            continue
-        for y in range(y_min, y_max):
-            if fold(p, y, y_axis = True):
-                total += (y + 1) * 100
-                is_folded = True
-                break
+    for n in  notes:
+        total += n[0] + 1 if n[0] is not None else (n[1] + 1) * 100
     return total
 
+def part1(patterns):
+    reflection_lines = []
+    for p in patterns:
+        p_lines = find_reflection(p)
+        assert(len(p_lines) == 1) # only 1 reflection line per pattern in part 1
+        reflection_lines.extend(p_lines)
+    return summarize(reflection_lines)
+
 def part2(patterns):
-    return None
+    reflection_lines = []
+    for p in patterns:
+        original_lines = find_reflection(p)
+        current_lines = set()
+        x_min, x_max, y_min, y_max = grid_size(p)
+        for y_fix in range(0, y_max+1):
+            for x_fix in range(0, x_max+1):
+                # since we only store ROCK coordinates
+                # this assumes there are no empty (i.e. ASH only) rows/cols
+                # at the extremities of the patterns -> not always true
+                p_fixed = copy(p)
+                # fix a smudge at x,y
+                if (x_fix,y_fix) in p_fixed:
+                    p_fixed.pop((x_fix,y_fix))
+                else:
+                    p_fixed[(x_fix,y_fix)] = ROCK
+                current_lines.update(find_reflection(p_fixed))
+        reflection_lines.extend(f for f in current_lines.difference(original_lines))
+    return summarize(reflection_lines)
 
 
 if __name__ == '__main__':
