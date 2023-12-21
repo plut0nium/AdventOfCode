0144b776edd9e76dd048e11d2eb642c9fbab388c
Charles Fourneau <charles.fourneau@gmail.com>
2023-12-21 10:34:01 +0100

    2023.21 part 2 - work in progress - NOT working

--- 2023/21/day_21.py
+++ 2023/21/day_21.py
@@ -23,12 +23,36 @@ def parse_garden(input_lines):
                 garden[x,y] = p
             elif p == START:
                 start_pos = (x,y)
-    garden_size = (x+1,y+1)
-    return garden, start_pos, garden_size
+    return garden, start_pos
+
+def grid_size(g):
+    x, y = map(set, zip(*g.keys()))
+    return min(x), max(x), min(y), max(y)
+
+def garden_size(g):
+    # garden has a blank padding around it
+    x_min, x_max, y_min, y_max = grid_size(g)
+    return x_min-1, x_max+1, y_min-1, y_max+1 
 
 @timing
-def part1(garden, start_pos, size, steps=6):
+def part1(garden, start_pos, steps=6):
     pos = [{start_pos}]
+    x_min, x_max, y_min, y_max = garden_size(garden)
+    for s in range(steps):
+        pos.append(set())
+        for p in pos[-2]:
+            for d in DIRS.values():
+                pos_next = (p[0]+d[0], p[1]+d[1])
+                if x_min <= pos_next[0] <= x_max \
+                   and y_min <= pos_next[1] <= y_max \
+                   and pos_next not in garden:
+                    pos[-1].add(pos_next)
+    return len(pos[-1])
+
+@timing
+def part2(garden, start_pos, steps=10):
+    pos = [{start_pos}]
+    x_min, x_max, y_min, y_max = garden_size(garden)
     for s in range(steps):
         pos.append(set())
         for p in pos[-2]:
@@ -38,13 +62,9 @@ def part1(garden, start_pos, size, steps=6):
                     pos[-1].add(pos_next)
     return len(pos[-1])
 
-@timing
-def part2(garden):
-    return None
-
 
 if __name__ == '__main__':
     with open(input_file, 'r') as f:
-        garden, start_pos, size = parse_garden(f.readlines())
-    print("Part #1 :", part1(garden, start_pos, size, 64))
-    print("Part #2 :", part2(garden))
+        garden, start_pos = parse_garden(f.readlines())
+    print("Part #1 :", part1(garden, start_pos, 64))
+    print("Part #2 :", part2(garden, start_pos))
