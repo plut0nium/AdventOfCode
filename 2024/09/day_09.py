adaced10ccbd2012a96d83a75de5910d712c9fd7
Charles Fourneau <charles.fourneau@proton.me>
2024-12-09 10:38:25 +0100

    2024.09 part 2

--- 2024/09/day_09.py
+++ 2024/09/day_09.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 input_file = "input"
-input_file = "test01.txt"
+# input_file = "test01.txt"
 # input_file = "test02.txt"
 
 
@@ -29,11 +29,41 @@ def part1(disk_map):
 
 def part2(disk_map):
 
-    return None
+    def find_next_free_space(disk_map, min_size=0):
+        for i, b in enumerate(disk_map):
+            if b[1] == FREE_SPACE \
+               and b[0] >= min_size:
+                return i
+        return None
+
+    def unpack(disk_map):
+        unpacked = []
+        for b in disk_map:
+            unpacked.extend([b[1]] * b[0])
+        return unpacked
+
+    for b in [b for b in disk_map[::-1] if b[1] != FREE_SPACE]:
+        file_index = disk_map.index(b)
+        next_free_space = find_next_free_space(disk_map, b[0])
+        if next_free_space is None \
+           or next_free_space >= file_index:
+            # no available space
+            continue
+        if disk_map[next_free_space][0] == b[0]:
+            #replace
+            disk_map[next_free_space] = b
+            disk_map[file_index] = (b[0], FREE_SPACE) # could merge free spaces
+        else:
+            #split
+            disk_map.insert(next_free_space, b)
+            disk_map[next_free_space+1] = (disk_map[next_free_space+1][0] - b[0], FREE_SPACE)
+            disk_map[file_index+1] = (b[0], FREE_SPACE)
+    return checksum(unpack(disk_map))
 
 
 if __name__ == '__main__':
     disk_map = []
+    disk_map2 = []
     with open(input_file, 'r') as f:
         map_str = f.readline().strip()
         file_id = 0
@@ -45,5 +75,6 @@ if __name__ == '__main__':
                 file_id += 1
             for j in range(int(c)):
                 disk_map.append(content)
+            disk_map2.append((int(c), content))
     print(part1(disk_map))
-    print(part2(disk_map))
+    print(part2(disk_map2))
