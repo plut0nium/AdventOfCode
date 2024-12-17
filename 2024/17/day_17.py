29b89f6f69b27f4f1acdeed7d41382945dfd017c
Charles Fourneau <charles.fourneau@proton.me>
2024-12-17 12:23:08 +0100

    2024.17 part 2

--- 2024/17/day_17.py
+++ 2024/17/day_17.py
@@ -3,14 +3,23 @@
 
 input_file = "input"
 # input_file = "test01.txt"
-input_file = "test02.txt"
+# input_file = "test02.txt"
 # input_file = "test03.txt"
 
-from collections import defaultdict
-from itertools import chain
 import re
 
 digit_re = re.compile(r'\d+')
+
+
+def combo(operand, registers):
+    # operand
+    if 0 <= operand <= 3:
+        return operand
+    elif 4 <= operand <= 6:
+        return registers[operand - 4]
+    else:
+        raise ValueError(f"Unsupported combo operand: {operand}")
+    return None
 
 
 def run(program, registers):
@@ -21,21 +30,13 @@ def run(program, registers):
             break
         opcode = program[ip]
         operand = program[ip+1]
-        # operand
-        if 0 <= operand <= 3:
-            operand_value = operand
-        elif 4 <= operand <= 6:
-            operand_value = registers[operand - 4]
-        else:
-            operand_value = None
-            # raise ValueError(f"Unsupported operand: {operand}")
         # program
         if opcode == 0: # adv
-            registers[0] = int(registers[0] / (2.0 ** operand_value))
+            registers[0] = int(registers[0] / (2.0 ** combo(operand, registers)))
         elif opcode == 1: # bxl
             registers[1] = registers[1] ^ operand
         elif opcode == 2: # bst
-            registers[1] = operand_value % 8
+            registers[1] = combo(operand, registers) % 8
         elif opcode == 3: # jnz
             if registers[0] != 0:
                 ip = operand
@@ -43,11 +44,11 @@ def run(program, registers):
         elif opcode == 4: # bxc
             registers[1] = registers[1] ^ registers[2]
         elif opcode == 5: # out
-            output.append(operand_value % 8)
+            output.append(combo(operand, registers) % 8)
         elif opcode == 6: # bdv
-            registers[1] = int(registers[0] / (2.0 ** operand_value))
+            registers[1] = int(registers[0] / (2.0 ** combo(operand, registers)))
         elif opcode == 7: # cdv
-            registers[2] = int(registers[0] / (2.0 ** operand_value))
+            registers[2] = int(registers[0] / (2.0 ** combo(operand, registers)))
         else:
             raise ValueError(f"Unsupported opcode: {opcode}")
         ip += 2
@@ -58,12 +59,24 @@ def part1(program, registers):
     return ",".join(str(v) for v in run(program, registers))
 
 
-def part2(program, registers):
-    return ",".join(str(v) for v in run(program, registers))
+def part2(program):
+    queue = [(1, 0)]
+    while len(queue):
+        nb_digits, register_a = queue.pop(0)
+        for i in range(8):
+            register_a_new = (register_a << 3) + i # assumes all programs have adv(3)
+            result = run(program, [register_a_new, 0, 0])
+            if result == program[len(program) - nb_digits:]:
+                # got the last nb_digits right
+                if len(program) == nb_digits:
+                    # all digits right !
+                    return register_a_new
+                queue.append((nb_digits + 1, register_a_new))
+    return None
 
 
 def part2_brute(program, registers):
-    register_a = 0
+    register_a = registers[0]
     while True:
         registers = [register_a, 0, 0]
         output = run(program, registers)
@@ -79,5 +92,5 @@ if __name__ == '__main__':
         r, p = f.read().split("\n\n")
         registers = [int(d) for d in digit_re.findall(r)]
         program = [int(o) for o in p.split(":")[1].strip().split(",")]
-    print(part1(program, registers))
-    print(part2_brute(program, registers))
+    print(part1(program, registers[:]))
+    print(part2(program))
