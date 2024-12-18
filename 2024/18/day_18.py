7f1fa381d29735a9c273f3fe725c217c08bd88e5
Charles Fourneau <charles.fourneau@proton.me>
2024-12-18 09:18:28 +0100

    2024.18 part 2 - works on test sample, NOT on real input...

--- 2024/18/day_18.py
+++ 2024/18/day_18.py
@@ -14,6 +14,8 @@ from utils import timing
 
 DIRS = [ ( 0,-1), ( 1, 0), ( 0, 1), (-1, 0) ] # N > E > S > W
 
+BLOCK = "#"
+EMPTY = "."
 START = (0, 0)
 if "test" in input_file:
     BYTE_COUNT = 12
@@ -27,20 +29,18 @@ def print_grid(grid, size, path=None):
     for y in range(size[1] + 1):
         for x in range(size[0] + 1):
             if (x,y) in grid:
-                print("#", end="")
+                print(BLOCK, end="")
             elif path is not None \
                and (x,y) in path:
                 print("o", end="")
             else:
-                print(".", end="")
+                print(EMPTY, end="")
         print()
     return None
 
 
-@timing
-def part1(byte_list):
-    grid = {b:"#" for b in byte_list[:BYTE_COUNT]}
-    queue = [[START], ]
+def find_path(grid, start, end):
+    queue = [[start], ]
     visited = {}
     while len(queue):
         path = queue.pop(0)
@@ -48,8 +48,8 @@ def part1(byte_list):
         for d in DIRS:
             x_next = x + d[0]
             y_next = y + d[1]
-            if not (0 <= x_next <= END[0]) \
-               or not (0 <= y_next <= END[1]):
+            if not (0 <= x_next <= end[0]) \
+               or not (0 <= y_next <= end[1]):
                 continue
             if (x_next, y_next) in path \
                or (x_next, y_next) in grid:
@@ -58,16 +58,30 @@ def part1(byte_list):
                and visited[(x_next, y_next)] <= len(path) + 1:
                 continue
             visited[(x_next, y_next)] = len(path) + 1
-            if (x_next, y_next) == END:
-                return len(path + [(x_next, y_next)]) - 1
+            if (x_next, y_next) == end:
+                return path + [(x_next, y_next)]
             else:
                 queue.append(path + [(x_next, y_next)])
     return None
 
 
 @timing
-def part2(byte_list):
+def part1(byte_list):
+    grid = {b:BLOCK for b in byte_list[:BYTE_COUNT]}
+    return len(find_path(grid, START, END)) - 1
 
+
+@timing
+def part2(byte_list):
+    grid = {b:BLOCK for b in byte_list[:BYTE_COUNT]}
+    path = find_path(grid, START, END)
+    for b in byte_list[BYTE_COUNT:]:
+        if b not in path:
+            continue
+        grid[b] = BLOCK
+        path = find_path(grid, START, END)
+        if path is None:
+            return ",".join(map(str, b))
     return None
 
 
